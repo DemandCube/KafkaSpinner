14dc4e8b4684d2579d4b3c50645e0d90cb29e89a
Peter Jerold Leslie <jeroldleslie@gmail.com>
2014-12-05 00:37:24 +0530

    Delete config_modifier.sh

--- config_modifier.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#BROKER_ID=4
-sed -e "s/broker.id=0/broker.id=$BROKER_ID/;s/#host.name=localhost/host.name=knode$BROKER_ID/;s/zookeeper.connect=localhost:2181/zookeeper.connect=znode:2181/" /opt/kafka/config/server.properties > /opt/kafka/config/tmp.properties
-sed 's/??/\
-/g' /opt/kafka/config/tmp.properties > /opt/kafka/config/server.properties
-rm /opt/kafka/config/tmp.properties
-
