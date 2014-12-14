ba6ded63604276542c5952de7bf574477b31945b
Peter Jerold Leslie <leslie@charlietech.com>
2014-12-14 14:27:45 +0530

    install latest docker for modify /etc/hosts

--- config-zookeeper.sh
+++ config-zookeeper.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#mkdir -p ~/.ssh;
-#mv /opt/authorized_keys ~/.ssh/authorized_keys;
-#chmod 700 ~/.ssh;
-#chmod 644 ~/.ssh/authorized_keys;
 
 echo "$SERVER_ID" > /tmp/zookeeper/myid  
 
