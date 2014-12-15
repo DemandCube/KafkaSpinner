3ec4354679d29a721fbbde440e1be5cae04b072e
KingOfPoptart <richard.c.duarte@gmail.com>
2014-12-15 12:45:57 +0000

    Fixing permission issue, can now run as Vagrant user

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,5 @@
 set -x
 
-#This is to allow passwordless ssh with root
-#No longer needed
-#sudo su - root
-#ssh-keygen -f /root/.ssh/id_rsa -t rsa -N ''
-#cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
-#echo "root ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
-#sed -i 's/^PermitRootLogin without-password/PermitRootLogin yes/' /etc/ssh/sshd_config
-#service ssh reload
-#service ssh restart
-
 #Install Docker
 sudo add-apt-repository ppa:cwchien/gradle
 sudo apt-get update
@@ -37,13 +27,13 @@ sudo groupadd docker
 #sudo su - kafkaspinner -c "chmod 600 /home/kafkaspinner/.ssh/*"
 
 #Add vagrant user to docker group
-#sudo gpasswd -a vagrant docker
+sudo gpasswd -a vagrant docker
 
 #Set up new public key for vagrant user
-#mkdir /home/vagrant/.ssh
-#ssh-keygen -f /home/vagrant/.ssh/id_rsa -t rsa -N ''
-#chmod 700 /home/vagrant/.ssh
-#chmod 600 /home/vagrant/.ssh/*
+sudo su - vagrant -c "mkdir /home/vagrant/.ssh"
+sudo su - vagrant -c "ssh-keygen -f /home/vagrant/.ssh/id_rsa -t rsa -N ''"
+sudo su - vagrant -c "chmod 700 /home/vagrant/.ssh"
+sudo su - vagrant -c "chmod 600 /home/vagrant/.ssh/*"
 
 
 #Add root user to docker group
