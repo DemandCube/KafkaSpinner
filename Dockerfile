ed0f472c245d007267702402d6efb09f7e5d1d98
Peter Jerold Leslie <leslie@charlietech.com>
2014-12-18 16:01:44 +0530

    clean and dirty shutdown

--- Dockerfile
+++ Dockerfile
@@ -14,16 +14,20 @@ ENV NOTVISIBLE "in users profile"
 RUN echo "export VISIBLE=now" >> /etc/profile
 
 
-RUN wget -q http://apache.01link.hk/kafka/0.8.1.1/kafka_2.9.2-0.8.1.1.tgz -O /tmp/kafka_2.9.2-0.8.1.1.tgz
-RUN tar xfz /tmp/kafka_2.9.2-0.8.1.1.tgz -C /opt
-RUN mv /opt/kafka_2.9.2-0.8.1.1 /opt/kafka
+#RUN wget -q http://apache.01link.hk/kafka/0.8.1.1/kafka_2.9.2-0.8.1.1.tgz -O /tmp/kafka_2.9.2-0.8.1.1.tgz
+#RUN tar xfz /tmp/kafka_2.9.2-0.8.1.1.tgz -C /opt
+#RUN mv /opt/kafka_2.9.2-0.8.1.1 /opt/kafka
+
+RUN wget -q http://apache.01link.hk/kafka/0.8.2-beta/kafka_2.10-0.8.2-beta.tgz -O /tmp/kafka_2.10-0.8.2-beta.tgz 
+RUN tar xfz /tmp/kafka_2.10-0.8.2-beta.tgz -C /opt
+RUN mv /opt/kafka_2.10-0.8.2-beta /opt/kafka
 
 RUN wget -q -O - http://apache.mirrors.pair.com/zookeeper/zookeeper-3.4.6/zookeeper-3.4.6.tar.gz | tar -xzf - -C /opt
 RUN mv /opt/zookeeper-3.4.6 /opt/zookeeper
 RUN cp /opt/zookeeper/conf/zoo_sample.cfg /opt/zookeeper/conf/zoo.cfg
 RUN mkdir -p /tmp/zookeeper
 
-
+#ADD configs/kafka-server-stop.sh /opt/kafka/bin/kafka-server-stop.sh
 ADD start-kafka.sh /opt/kafka/start-kafka.sh
 ADD start-zookeeper.sh /opt/zookeeper/start-zookeeper.sh
 
@@ -44,10 +48,11 @@ ADD authorized_keys /root/.ssh/authorized_keys
 RUN chmod 644 /root/.ssh/authorized_keys
 
 ADD zoo.cfg /opt/zookeeper/conf/zoo.cfg
-
+ADD configs/server.sample /opt/kafka/config/server.properties 
 
 ENV KAFKA_HOME /opt/kafka
 
 EXPOSE 2181 9092 22
 
-#CMD ["/usr/sbin/sshd", "-D"]
+
+#CMD ["/usr/sbin/sshd", "-D", "FOREGROUND"]
--- cluster-helper.sh
+++ cluster-helper.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 
+CLUSTER_CONTAINER_LIST+=($(docker ps -a -q))
 
 # This function is used to display usage.
 function usage
@@ -8,12 +9,12 @@ function usage
   echo -e "\nKafka Spinner Cluster operations usage\n"
   printf "%s\t%s\n" "--ip" "Print list of IP address and hostname on the cluster"  
   printf "%s\t\t\t%s\n" "--ssh" "SSH into any node of the cluster. eg: --ssh nodename"      
-  printf "%s\t\t%s\n" "--help | -h" "Help"  
+  printf "%s\t\t%s\n" "--help | -h" "Help"
+  echo "--modify-hosts    modify hosts file of all nodes (eg: ----modify-hosts nodename)"  
 }
 
 function printClusterInfo
 {
-CLUSTER_CONTAINER_LIST+=($(docker ps -a -q))
 printf "%s\t%s\n" "HOSTNAME" "IP ADDRESS"
 for i in "${CLUSTER_CONTAINER_LIST[@]}"
 do
@@ -26,12 +27,32 @@ function sshNode
 ssh -o StrictHostKeyChecking=no root@$(docker inspect -f '{{ .NetworkSettings.IPAddress }}' $1)
 }
 
+
+function modifyHosts
+{
+  echo "Modifying /etc/hosts..."
+  for i in "${CLUSTER_CONTAINER_LIST[@]}"
+    do
+            ssh -o StrictHostKeyChecking=no root@$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" $i) "cp /etc/hosts /etc/hosts.bak && sed -e '/"$1"/s=^[0-9\.]*='"$(docker inspect -f '{{ .NetworkSettings.IPAddress }}' $1)"'=' /etc/hosts.bak > /etc/hosts"
+    done
+
+      for j in "${CLUSTER_CONTAINER_LIST[@]}"
+        do
+          ssh -o StrictHostKeyChecking=no root@$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" $1) "echo '$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" $j)   $(docker inspect -f '{{ .Config.Hostname }}' $j)'  >> /etc/hosts"
+        done
+}
+
+
+
 while [ "$1" != "" ]; do
   case $1 in
     --ssh)                   shift
                              sshNode $1
                              ;;
-    --ip)      printClusterInfo
+    --ip)                    printClusterInfo
+                             ;;
+    --modify-hosts)          shift
+                             modifyHosts $1
                              ;;
     -h | --help )            usage
                              exit
